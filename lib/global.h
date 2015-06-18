3997f947a4dce40f4a425994a9cb6dffc540a82a
Anthony Zheng <tamakimouto@gmail.com>
2015-06-17 20:41:46 -0400

    uploader

---
+++ lib/global.h
@@ -0,0 +1,6 @@
+#include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+#include <glib/gstdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
---
+++ lib/uploader.h
@@ -0,0 +1,8 @@
+#include <curl/curl.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+
+#include "global.h"
+
+char * 
+upload (gchar * path);
