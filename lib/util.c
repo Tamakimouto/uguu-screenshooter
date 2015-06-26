9b0d48a79111d264c0e5f07344cc04b176ba7e99
Anthony Zheng <tamakimouto@gmail.com>
2015-06-26 19:56:46 -0400

    to clip function

--- lib/util.c
+++ lib/util.c
@@ -1,1 +1,10 @@
 #include "global.h"
+
+void
+copyToClipboard (char * url){
+
+  GtkClipboard * board;  
+  board = gtk_clipboard_get (GDK_SELECTION_PRIMARY);
+  gtk_clipboard_set_text (board, url, -1);
+
+}//copy to clipboard
